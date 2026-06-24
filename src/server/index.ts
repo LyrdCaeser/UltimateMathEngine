befcd52ddb9bd524c9d1d12a975a70dc27dfc8d1
Lyrd Caeser <huuxhoang@gmail.com>
2026-06-24 09:15:20 +0700

    Update index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -33,10 +33,8 @@ app.use('/api', gameRatesRouter);
 app.use('/api/settings', settingsRouter);
 
 const distPath = join(process.cwd(), 'dist');
-
 if (existsSync(distPath)) {
   app.use(express.static(distPath));
-
   app.get(/.*/, (_req, res) => {
     res.sendFile(join(distPath, 'index.html'));
   });
